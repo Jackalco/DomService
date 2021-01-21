b3a9203d55e382e43051d9ce16aded539ca753ce
Jackalco <vincent.jacques@viacesi.fr>
2021-01-21 13:56:03 +0100

    Start the home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('css/layouts.css')}}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/home.css')}}">
         <script src="https://kit.fontawesome.com/172e84d6d0.js" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="{{ asset('js/carousel.js') }}"></script>
 
         <title>DOM Service</title>
 
@@ -13,9 +14,74 @@
     <body>
         @include('layouts.nav')
         <main>
-            <div>
-                Accueil
-            </div>
+            <section class="demoLeft">
+                <img src="{{ asset('images/img1.jpg') }}" class="imageDemo" alt="img1">
+                <div class="textDemo">
+                    <h2>Titre</h2>
+                    <hr>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat sapien ornare, ultricies dui sed, lobortis nisl. In odio enim, condimentum in pharetra ac, faucibus sed odio. Duis pharetra erat in libero sagittis, in luctus magna sagittis. Ut augue arcu, congue in tristique sit amet, gravida ac nunc. Duis sollicitudin nulla dolor, at mattis libero congue id. Etiam ornare, urna sed pharetra sodales, diam lectus posuere velit, id facilisis elit nibh vitae tellus. Integer ut lorem id lacus pellentesque gravida. Fusce pellentesque ut magna sed consectetur. Phasellus leo felis, luctus at est vitae, lacinia vehicula dolor. Quisque at feugiat enim.
+                    </p>
+                    <br>
+                    <a class="buttonAbout" href="">En savoir plus</a>
+                </div>
+            <!--<div class="slideShowContainer">
+                    <div class="slides fade">
+                        <img src="{{ asset('images/img1.jpg') }}" class="imgSlide">
+                        <div class="text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat sapien ornare, ultricies dui sed, lobortis nisl. In odio enim, condimentum in pharetra ac, faucibus sed odio. Duis pharetra erat in libero sagittis, in luctus magna sagittis. Ut augue arcu, congue in tristique sit amet, gravida ac nunc. Duis sollicitudin nulla dolor, at mattis libero congue id. Etiam ornare, urna sed pharetra sodales, diam lectus posuere velit, id facilisis elit nibh vitae tellus. Integer ut lorem id lacus pellentesque gravida. Fusce pellentesque ut magna sed consectetur. Phasellus leo felis, luctus at est vitae, lacinia vehicula dolor. Quisque at feugiat enim.
+                        </div>
+                    </div>
+                        
+                    </div>
+                    <div class="slides fade">
+                        <img src="{{ asset('images/img2.jpg') }}" class="imgSlide">
+                        <div class="text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat sapien ornare, ultricies dui sed, lobortis nisl. In odio enim, condimentum in pharetra ac, faucibus sed odio. Duis pharetra erat in libero sagittis, in luctus magna sagittis. Ut augue arcu, congue in tristique sit amet, gravida ac nunc. Duis sollicitudin nulla dolor, at mattis libero congue id. Etiam ornare, urna sed pharetra sodales, diam lectus posuere velit, id facilisis elit nibh vitae tellus. Integer ut lorem id lacus pellentesque gravida. Fusce pellentesque ut magna sed consectetur. Phasellus leo felis, luctus at est vitae, lacinia vehicula dolor. Quisque at feugiat enim.
+                        </div>
+                    </div>
+                    <div class="slides fade">
+                        <img src="{{ asset('images/img3.jpg') }}" class="imgSlide">
+                        <div class="text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat sapien ornare, ultricies dui sed, lobortis nisl. In odio enim, condimentum in pharetra ac, faucibus sed odio. Duis pharetra erat in libero sagittis, in luctus magna sagittis. Ut augue arcu, congue in tristique sit amet, gravida ac nunc. Duis sollicitudin nulla dolor, at mattis libero congue id. Etiam ornare, urna sed pharetra sodales, diam lectus posuere velit, id facilisis elit nibh vitae tellus. Integer ut lorem id lacus pellentesque gravida. Fusce pellentesque ut magna sed consectetur. Phasellus leo felis, luctus at est vitae, lacinia vehicula dolor. Quisque at feugiat enim.
+                        </div>
+                    </div>
+                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                </div>
+                <div style="text-align:center">
+                    <span class="dot" onclick="currentSlide(1)"></span>
+                    <span class="dot" onclick="currentSlide(2)"></span>
+                    <span class="dot" onclick="currentSlide(3)"></span>
+                </div> -->
+            </section>
+            <section class="demoRight">
+                <div class="textDemo">
+                    <h2>Titre</h2>
+                    <hr>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat sapien ornare, ultricies dui sed, lobortis nisl. In odio enim, condimentum in pharetra ac, faucibus sed odio. Duis pharetra erat in libero sagittis, in luctus magna sagittis. Ut augue arcu, congue in tristique sit amet, gravida ac nunc. Duis sollicitudin nulla dolor, at mattis libero congue id. Etiam ornare, urna sed pharetra sodales, diam lectus posuere velit, id facilisis elit nibh vitae tellus. Integer ut lorem id lacus pellentesque gravida. Fusce pellentesque ut magna sed consectetur. Phasellus leo felis, luctus at est vitae, lacinia vehicula dolor. Quisque at feugiat enim.
+                    </p>
+                    <br>
+                    <a class="buttonAbout" href="">En savoir plus</a>
+                </div>
+                <img src="{{ asset('images/img2.jpg') }}" class="imageDemo" alt="img2">   
+            </section>
+            <section class="demoLeft">
+                <img src="{{ asset('images/img3.jpg') }}" class="imageDemo" alt="img3">
+                <div class="textDemo">
+                    <h2>Titre</h2>
+                    <hr>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat sapien ornare, ultricies dui sed, lobortis nisl. In odio enim, condimentum in pharetra ac, faucibus sed odio. Duis pharetra erat in libero sagittis, in luctus magna sagittis. Ut augue arcu, congue in tristique sit amet, gravida ac nunc. Duis sollicitudin nulla dolor, at mattis libero congue id. Etiam ornare, urna sed pharetra sodales, diam lectus posuere velit, id facilisis elit nibh vitae tellus. Integer ut lorem id lacus pellentesque gravida. Fusce pellentesque ut magna sed consectetur. Phasellus leo felis, luctus at est vitae, lacinia vehicula dolor. Quisque at feugiat enim.
+                    </p>
+                    <br>
+                    <a class="buttonAbout" href="">En savoir plus</a>
+                </div>
+            </section>
+            <section class="linkFindAgencies">
+
+            </section>
         </main>
         @include('layouts.footer')
     </body>
--- storage/framework/views/1a9978ebefe27feccec373dcb28bc264829cd178.php
+++ storage/framework/views/1a9978ebefe27feccec373dcb28bc264829cd178.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" type="text/css" href="<?php echo e(asset('css/layouts.css')); ?>">
         <link rel="stylesheet" type="text/css" href="<?php echo e(asset('css/home.css')); ?>">
         <script src="https://kit.fontawesome.com/172e84d6d0.js" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="<?php echo e(asset('js/carousel.js')); ?>"></script>
 
         <title>DOM Service</title>
 
@@ -13,9 +14,71 @@
     <body>
         <?php echo $__env->make('layouts.nav', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
         <main>
-            <div>
-                Accueil
-            </div>
+            <section class="demoLeft">
+                <img src="<?php echo e(asset('images/img1.jpg')); ?>" class="imageDemo" alt="img1">
+                <div class="textDemo">
+                    <h2>Titre</h2>
+                    <hr>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat sapien ornare, ultricies dui sed, lobortis nisl. In odio enim, condimentum in pharetra ac, faucibus sed odio. Duis pharetra erat in libero sagittis, in luctus magna sagittis. Ut augue arcu, congue in tristique sit amet, gravida ac nunc. Duis sollicitudin nulla dolor, at mattis libero congue id. Etiam ornare, urna sed pharetra sodales, diam lectus posuere velit, id facilisis elit nibh vitae tellus. Integer ut lorem id lacus pellentesque gravida. Fusce pellentesque ut magna sed consectetur. Phasellus leo felis, luctus at est vitae, lacinia vehicula dolor. Quisque at feugiat enim.
+                    </p>
+                    <br>
+                    <a class="buttonAbout" href="">En savoir plus</a>
+                </div>
+            <!--<div class="slideShowContainer">
+                    <div class="slides fade">
+                        <img src="<?php echo e(asset('images/img1.jpg')); ?>" class="imgSlide">
+                        <div class="text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat sapien ornare, ultricies dui sed, lobortis nisl. In odio enim, condimentum in pharetra ac, faucibus sed odio. Duis pharetra erat in libero sagittis, in luctus magna sagittis. Ut augue arcu, congue in tristique sit amet, gravida ac nunc. Duis sollicitudin nulla dolor, at mattis libero congue id. Etiam ornare, urna sed pharetra sodales, diam lectus posuere velit, id facilisis elit nibh vitae tellus. Integer ut lorem id lacus pellentesque gravida. Fusce pellentesque ut magna sed consectetur. Phasellus leo felis, luctus at est vitae, lacinia vehicula dolor. Quisque at feugiat enim.
+                        </div>
+                    </div>
+                        
+                    </div>
+                    <div class="slides fade">
+                        <img src="<?php echo e(asset('images/img2.jpg')); ?>" class="imgSlide">
+                        <div class="text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat sapien ornare, ultricies dui sed, lobortis nisl. In odio enim, condimentum in pharetra ac, faucibus sed odio. Duis pharetra erat in libero sagittis, in luctus magna sagittis. Ut augue arcu, congue in tristique sit amet, gravida ac nunc. Duis sollicitudin nulla dolor, at mattis libero congue id. Etiam ornare, urna sed pharetra sodales, diam lectus posuere velit, id facilisis elit nibh vitae tellus. Integer ut lorem id lacus pellentesque gravida. Fusce pellentesque ut magna sed consectetur. Phasellus leo felis, luctus at est vitae, lacinia vehicula dolor. Quisque at feugiat enim.
+                        </div>
+                    </div>
+                    <div class="slides fade">
+                        <img src="<?php echo e(asset('images/img3.jpg')); ?>" class="imgSlide">
+                        <div class="text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat sapien ornare, ultricies dui sed, lobortis nisl. In odio enim, condimentum in pharetra ac, faucibus sed odio. Duis pharetra erat in libero sagittis, in luctus magna sagittis. Ut augue arcu, congue in tristique sit amet, gravida ac nunc. Duis sollicitudin nulla dolor, at mattis libero congue id. Etiam ornare, urna sed pharetra sodales, diam lectus posuere velit, id facilisis elit nibh vitae tellus. Integer ut lorem id lacus pellentesque gravida. Fusce pellentesque ut magna sed consectetur. Phasellus leo felis, luctus at est vitae, lacinia vehicula dolor. Quisque at feugiat enim.
+                        </div>
+                    </div>
+                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                </div>
+                <div style="text-align:center">
+                    <span class="dot" onclick="currentSlide(1)"></span>
+                    <span class="dot" onclick="currentSlide(2)"></span>
+                    <span class="dot" onclick="currentSlide(3)"></span>
+                </div> -->
+            </section>
+            <section class="demoRight">
+                <div class="textDemo">
+                    <h2>Titre</h2>
+                    <hr>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat sapien ornare, ultricies dui sed, lobortis nisl. In odio enim, condimentum in pharetra ac, faucibus sed odio. Duis pharetra erat in libero sagittis, in luctus magna sagittis. Ut augue arcu, congue in tristique sit amet, gravida ac nunc. Duis sollicitudin nulla dolor, at mattis libero congue id. Etiam ornare, urna sed pharetra sodales, diam lectus posuere velit, id facilisis elit nibh vitae tellus. Integer ut lorem id lacus pellentesque gravida. Fusce pellentesque ut magna sed consectetur. Phasellus leo felis, luctus at est vitae, lacinia vehicula dolor. Quisque at feugiat enim.
+                    </p>
+                    <br>
+                    <a class="buttonAbout" href="">En savoir plus</a>
+                </div>
+                <img src="<?php echo e(asset('images/img2.jpg')); ?>" class="imageDemo" alt="img2">   
+            </section>
+            <section class="demoLeft">
+                <img src="<?php echo e(asset('images/img3.jpg')); ?>" class="imageDemo" alt="img3">
+                <div class="textDemo">
+                    <h2>Titre</h2>
+                    <hr>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat sapien ornare, ultricies dui sed, lobortis nisl. In odio enim, condimentum in pharetra ac, faucibus sed odio. Duis pharetra erat in libero sagittis, in luctus magna sagittis. Ut augue arcu, congue in tristique sit amet, gravida ac nunc. Duis sollicitudin nulla dolor, at mattis libero congue id. Etiam ornare, urna sed pharetra sodales, diam lectus posuere velit, id facilisis elit nibh vitae tellus. Integer ut lorem id lacus pellentesque gravida. Fusce pellentesque ut magna sed consectetur. Phasellus leo felis, luctus at est vitae, lacinia vehicula dolor. Quisque at feugiat enim.
+                    </p>
+                    <br>
+                    <a class="buttonAbout" href="">En savoir plus</a>
+                </div>
+            </section>
         </main>
         <?php echo $__env->make('layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
     </body>
