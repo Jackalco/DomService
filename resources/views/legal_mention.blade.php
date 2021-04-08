5cbe8a0e09722909d390a07a2d5450c3422175c5
Jackalco <vincent.jacques@viacesi.fr>
2021-04-08 09:20:14 +0200

    Edit the audio file

--- resources/views/legal_mention.blade.php
+++ resources/views/legal_mention.blade.php
@@ -23,11 +23,12 @@
                 </div>
                 <div class="itemLegalMention">
                     <div class="infoLegalMention"><strong>Propriétaire :</strong> DOM Services</div>
+                    <div class="infoLegalMention"><strong>Adresse :</strong> 257 bis Avenue de Mindin, 44250 Saint-Brevin-les-Pins</div>
                     <div class="infoLegalMention"><strong>Activité :</strong> Service à domicile</div>
                     <div class="infoLegalMention"><strong>Capital social :</strong> 1000 €</div>
                     <div class="infoLegalMention"><strong>Numéro SIRET : </strong> 81117213900032</div>
                     <div class="infoLegalMention"><strong>Numéro de TVA :</strong> FR23811172139</div>
-                    <div class="infoLegalMention"><strong>Email :</strong> </div>
+                    <div class="infoLegalMention"><strong>Email :</strong> contact.saint-brevin@domservicesfrance.fr</div>
                     <div class="infoLegalMention"><strong>Téléphone :</strong> 09 72 58 61 65</div>
                     <div class="infoLegalMention"><strong>Webmasters :</strong> Vincent JACQUES</div>
                     <div class="infoLegalMention"><strong>Hébergeur :</strong> <a href="https://www.o2switch.fr/" target="_blank">02switch</a> – 224 BD GUSTAVE FLAUBERT
@@ -43,7 +44,7 @@
                     <h3>Gestion des données personelles</h3>
                     <p>En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier 1978, la loi n° 2004-801 du 6 août 2004, l’article L. 226-13 du Code pénal et la Directive Européenne du 24 octobre 1995.</p>
                     <p>A l’occasion de l’utilisation du site DOM Services, peuvent être recueillies : l’URL des liens par l’intermédiaire desquels l’utilisateur a accédé au site DOM Services, le fournisseur d’accès de l’utilisateur, l’adresse de protocole Internet (IP) de l’utilisateur.</p>
-                    <p>En tout état de cause DOM Services ne collecte des informations personnelles relatives à l’utilisateur que pour le besoin de certains services proposés par le site DOM Services (demande de contact, de devis…). L’utilisateur fournit ces informations en toute connaissance de cause, notamment lorsqu’il procède par lui-même à leur saisie. Il est alors précisé à l’utilisateur du site Senior Compagnie l’obligation ou non de fournir ces informations.</p>
+                    <p>En tout état de cause DOM Services ne collecte des informations personnelles relatives à l’utilisateur que pour le besoin de certains services proposés par le site DOM Services (demande de contact, de devis…). L’utilisateur fournit ces informations en toute connaissance de cause, notamment lorsqu’il procède par lui-même à leur saisie. Il est alors précisé à l’utilisateur du site DOM Services l’obligation ou non de fournir ces informations.</p>
                     <p>Conformément aux dispositions des articles 38 et suivants de la loi 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, tout utilisateur dispose d’un droit d’accès, de rectification et d’opposition aux données personnelles le concernant, en effectuant sa demande écrite et signée, accompagnée d’une copie du titre d’identité avec signature du titulaire de la pièce, en précisant l’adresse à laquelle la réponse doit être envoyée.</p>
                     <p>Aucune information personnelle de l’utilisateur du site DOM Services n’est publiée à l’insu de l’utilisateur, échangée, transférée, cédée ou vendue sur un support quelconque à des tiers.</p>
                     <p>Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.</p>
